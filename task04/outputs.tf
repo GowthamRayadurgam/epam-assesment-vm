772f6bff13f4527a67ab94aba1dc1a37076fd91b
Gowtham Rayadurgam <gowtham_rayadurgam@epam.com>
2025-04-03 17:16:36 +0530

    add

--- task04/outputs.tf
+++ task04/outputs.tf
@@ -1,18 +1,3 @@
-output "pip_id" {
-  value       = azurerm_public_ip.pip.id
-  description = "pip id"
-}
-
-output "subnet_id" {
-  value       = azurerm_subnet.subnet.id
-  description = "subnetid"
-}
-
-output "vm_public_ip" {
-  value       = azurerm_public_ip.pip.ip_address
-  description = "ipaddress"
-}
-
 output "vm_public_ipvm_fqdn" {
   value       = azurerm_public_ip.pip.fqdn
   description = "fqdn"
