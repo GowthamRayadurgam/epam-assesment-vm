68cebe7c10c3cba05c62a29b1967176528a67a6e
Gowtham Rayadurgam <gowtham_rayadurgam@epam.com>
2025-04-03 15:30:39 +0530

    code

---
+++ task04/outputs.tf
@@ -0,0 +1,7 @@
+output "pip_id" {
+  value = azurerm_public_ip.pip.id
+}
+
+output "subnet_id" {
+  value = azurerm_subnet.subnet.id
+}
---
+++ task04/variables.tf
@@ -0,0 +1,38 @@
+variable "vnetname" {
+}
+
+variable "location" {
+
+}
+
+variable "resource_group_name" {
+
+}
+
+variable "address_space" {
+
+}
+variable "subnetname" {
+
+}
+variable "address_prefix" {
+
+}
+
+variable "pip_name" {
+
+}
+
+variable "dns_lable" {
+
+}
+variable "nsg_name" {
+
+}
+variable "nicname" {
+
+}
+
+variable "vm_name" {
+
+}
