9c6f980cd21bd1378fb7eb70a0b9bc36b7622ed9
Gowtham Rayadurgam <gowtham_rayadurgam@epam.com>
2025-04-03 17:31:55 +0530

    add

--- task04/outputs.tf
+++ task04/outputs.tf
@@ -3,3 +3,7 @@ output "vm_fqdn" {
   description = "fqdn"
 }
 
+output "vm_public_ip" {
+  value       = azurerm_linux_virtual_machine.linux_vm.public_ip_address
+  description = "public ip of vm"
+}
